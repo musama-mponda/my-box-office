6a68d11dcb6b4c5a374cecc4024786a0abc9bfab
musama-mponda <musamamponda1997@gmail.com>
2021-05-29 16:13:26 +0530

    added loading and error messages to the show page

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,7 +1,9 @@
-const  API_BASE_URL = 'https://api.tvmaze.com';
+const API_BASE_URL = 'https://api.tvmaze.com';
 
 export async function apiGEt(queryString) {
-    const response = await fetch (`${API_BASE_URL}${queryString}`).then(r => r.json())
-    return response;
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  // throw new Error('OOPS');
+  return response;
 }
-
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,14 +6,39 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGEt(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGEt(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is been loaded</div>;
+  }
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
 
   return <div>this is show</div>;
 };
