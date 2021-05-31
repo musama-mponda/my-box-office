b0f27472c7e725e24d168e9dea5fe30600a7c2d4
musama-mponda <musamamponda1997@gmail.com>
2021-05-31 09:24:05 +0530

    using custom hooks for starred shows

--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import IMG_PLACEHOLDER from '../../images/not-found.png'
+import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Star } from '../styled';
 import { Headline, MainDataWrapper, TagList } from './ShowMainData.styled';
 
@@ -12,11 +12,14 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
         <Headline>
           <h1>{name}</h1>
           <div>
-            <Star />
+            <Star active />
             <span>{rating.average || 'N/A'}</span>
           </div>
         </Headline>
-        <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
+        <div
+          className="summary"
+          dangerouslySetInnerHTML={{ __html: summary }}
+        />
 
         <div>
           Tags:{' '}
