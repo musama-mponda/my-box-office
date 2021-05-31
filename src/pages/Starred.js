42e93c90fc87b34b1ecc80b39d12a23bce52fed6
musama-mponda <musamamponda1997@gmail.com>
2021-05-31 10:16:31 +0530

    finished starred page

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import { apiGEt } from '../misc/config';
+import { useShows } from '../misc/custom-hooks';
 
 const Starred = () => {
-  return <MainPageLayout>This is my starred</MainPageLayout>;
+  // displaying starred or favourite shows
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isloading, setIsloading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // to fetch data when the components mount we will use useEffect
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGEt(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsloading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsloading(false);
+        });
+    } else {
+      setIsloading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isloading && <div>Shows are still loading</div>}
+      {error && <div>Error Occured:{error}</div>}
+      {!isloading && !shows && <div>No Shows eere added</div>}
+      {!isloading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
